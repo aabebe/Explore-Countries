a14889266d91a246ffcc9f182feabc8ca0be60de
aabebe <aabebe@mum.edu>
2019-05-13 08:27:16 -0500

    implementaion of country controller

--- Backend/src/main/java/com/altimetric/playground/explorecountries/controller/CountryController.java
+++ Backend/src/main/java/com/altimetric/playground/explorecountries/controller/CountryController.java
@@ -1,10 +1,38 @@
 package com.altimetric.playground.explorecountries.controller;
 
+import com.altimetric.playground.explorecountries.service.CountryService;
+import com.altimetric.playground.explorecountries.service.ResponseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/countries")
 public class CountryController {
+
+    @Autowired
+    private CountryService countryService;
+    @Autowired
+    private ResponseService responseService;
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getCountries() {
+        return new ResponseEntity<>(countryService.getCountries(), HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<?> searchForCountry(@RequestParam("code") String code,
+                                              @RequestParam(required = false, value = "region") Optional<String> region,
+                                              @RequestParam(required = false, value = "incomeLevel") Optional<String> incomelevel,
+                                              @RequestParam(required = false, value = "lendingType") Optional<String> lendingType) {
+        return responseService.getResponse(countryService.getCountries(code, region, incomelevel, lendingType));
+    }
+
 
 }
