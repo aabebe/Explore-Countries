b4ee5b17ecf1f059a53d1d2540ee8de32466fc23
aabebe <aabebe@mum.edu>
2019-05-13 09:02:12 -0500

    after adding a consumer for the application

--- Backend/src/main/java/com/altimetric/playground/explorecountries/controller/CountryController.java
+++ Backend/src/main/java/com/altimetric/playground/explorecountries/controller/CountryController.java
@@ -5,10 +5,7 @@ import com.altimetric.playground.explorecountries.service.ResponseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -27,6 +24,7 @@ public class CountryController {
     }
 
     @GetMapping()
+    @CrossOrigin
     public ResponseEntity<?> searchForCountry(@RequestParam("code") String code,
                                               @RequestParam(required = false, value = "region") Optional<String> region,
                                               @RequestParam(required = false, value = "incomeLevel") Optional<String> incomelevel,
